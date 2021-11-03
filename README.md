3ce63ded4b4dfa40001a749be2f85df12007935d
n-yata <yata1214@gmail.com>
2021-11-03 15:56:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### 内容  
+PWAのサンプル  
+### 参考ソース  
+[PWAをもっと簡単に初めてみる](https://qiita.com/poster-keisuke/items/6651140fa20c7aa18474)
---
+++ service_worker.js
@@ -0,0 +1,28 @@
+// キャッシュファイルの指定
+var CHACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+    '/pwa-sample.github.io/',
+]
+
+// インストール処理
+self.addEventListener('install', function(event){
+    event.waitUntil(
+        caches
+        .open(CHACHE_NAME)
+        .then(function(cache){
+            return CHACHE_NAME.addAll(urlsToCache);
+        })
+    );
+});
+
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches
+        .match(event.request)
+        .then(function(response){
+            return response ? response : fetch(event.request);
+        })
+    );
+});
